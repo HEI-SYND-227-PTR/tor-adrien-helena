93ffdc1dbd5b0a25a02c5b96a5fe11d6180fc74c
Helena Syrbe <helenama.syrbe@hes-so.ch>
2024-04-18 15:46:53 +0200

    Token is built. Check if it really works.

--- mac_sender.c
+++ mac_sender.c
@@ -1,4 +1,3 @@
-
 #include "stm32f7xx_hal.h"
 
 #include "main.h"
@@ -7,48 +6,16 @@
 #include "rtx_os.h"
 
 
-
-
-	
-uint8_t* tokenPtr;
-	
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC SENDER
 //////////////////////////////////////////////////////////////////////////////////
 void MacSender(void *argument)
 {
-	// TODO
 	struct queueMsg_t queueMsg;				// queue message
 	char * stringPtr;									// string to send pointer
 	osStatus_t retCode;								// return error code
 	
-	uint8_t tokenFrame[17];
-
-
-	
-	//----------------------------------------------------------------------------
-	// TOKEN GENERATION
-	//----------------------------------------------------------------------------
-	for(int i = 0; i <=17; i++)
-	{
-		tokenFrame[i]=0;
-	}
-/*	token.st_0 = 0b00000000;
-	token.st_1 = 0b00000000;
-	token.st_2 = 0b00000000;
-	token.st_3 = 0b00001010;	//MYADDRESS	SAPI 1 for chat & SAPI 3 for time
-	token.st_4 = 0b00000000;
-	token.st_5 = 0b00000000;
-	token.st_6 = 0b00000000;
-	token.st_7 = 0b00000000;
-	token.st_8 = 0b00000000;
-	token.st_9 = 0b00000000;
-	token.st_10 = 0b00000000;
-	token.st_11 = 0b00000000;
-	token.st_12 = 0b00000000;
-	token.st_13 = 0b00000000;
-	token.st_14 = 0b00000000;
-	token.st_15 = 0b00000000;*/
+	uint8_t* tokenFrame;							// Points to the token frame
 	
 	//------------------------------------------------------------------------------
 	for (;;)														// loop until doomsday
@@ -61,26 +28,51 @@ void MacSender(void *argument)
 			&queueMsg,
 			NULL,
 			osWaitForever); 	
-//		queueMsg.type = ;
     CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
 		
-		switch(queueMsg.type){
-			case NEW_TOKEN:
-				queueMsg.anyPtr = &token;
-				queueMsg.type = TO_PHY;				
+		switch(queueMsg.type)
+		{
+			case (NEW_TOKEN):
+				//Create new token
+				tokenFrame = osMemoryPoolAlloc(memPool, osWaitForever); //get some memory in the pool
+				
+				//insert data into token frame
+				for(int i = 0; i < TOKENSIZE - 2; i++)
+				{
+					//reset the whole frame first
+					*(tokenFrame+i) = 0;
+				}
+				
+				//first byte is always 0xFF
+				*(tokenFrame) = 0xFF;
+				
+				//My station's byte has SAPIs 1 & 3 ready
+				//ATTENTION: BINARY WON'T WORK
+				*(tokenFrame + MYADDRESS) = 0x0A;
+				
+				//Reuse queueMsg, adjust type and dataPtr
+				queueMsg.anyPtr = tokenFrame;
+				queueMsg.type = TO_PHY;
+				
+				//Put to PHY_S Queue
 				retCode = osMessageQueuePut(queue_phyS_id, &queueMsg, osPriorityNormal,
 					osWaitForever);
-			    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
-
+			  CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
 			break;
-				
-				
+			/*
+			case (START):
+			break;
+			case (STOP):
+			break;
+			case (DATA_IND):
+			break;
+			case (TOKEN):
+			break;
+			case (DATABACK): //When WE are the sender and get an ACK, NACK or R, NR.. !
+			break;
+			default:
+			break;
+			*/
 		}
-		
 	}
-	
-	 
-
-
-	
 }
